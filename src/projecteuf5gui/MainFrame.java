f55eda4e540c674e39b59c89381a441fefe882a7
David Martinez <davidmgilo@gmail.com>
2016-11-21 17:01:54 +0100

    added panels workflow + classModelTaula

--- src/projecteuf5gui/MainFrame.java
+++ src/projecteuf5gui/MainFrame.java
@@ -5,6 +5,9 @@
  */
 package projecteuf5gui;
 
+import java.awt.event.WindowEvent;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author alumne
@@ -34,15 +37,27 @@ public class MainFrame extends javax.swing.JFrame {
         crudAlimentButton = new javax.swing.JButton();
         sortirButton = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
+        AlimentPanel = new javax.swing.JPanel();
+        AnimalPanel = new javax.swing.JPanel();
+        llistarPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setResizable(false);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                formWindowClosed(evt);
+            }
+        });
 
         jPanel1.setBackground(new java.awt.Color(24, 29, 47));
 
         jPanel2.setBackground(new java.awt.Color(24, 29, 47));
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Accions", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 0, 12), new java.awt.Color(254, 254, 254))); // NOI18N
+        jPanel2.setLayout(new java.awt.BorderLayout());
 
         llistarButton.setText("Llistar Animals");
         llistarButton.addActionListener(new java.awt.event.ActionListener() {
@@ -50,48 +65,80 @@ public class MainFrame extends javax.swing.JFrame {
                 llistarButtonActionPerformed(evt);
             }
         });
+        jPanel2.add(llistarButton, java.awt.BorderLayout.LINE_START);
 
         crudAnimalButton.setText("CRUD Animal");
+        crudAnimalButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                crudAnimalButtonActionPerformed(evt);
+            }
+        });
+        jPanel2.add(crudAnimalButton, java.awt.BorderLayout.CENTER);
 
         crudAlimentButton.setText("CRUD Aliment");
+        crudAlimentButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                crudAlimentButtonActionPerformed(evt);
+            }
+        });
+        jPanel2.add(crudAlimentButton, java.awt.BorderLayout.PAGE_START);
 
         sortirButton.setText("Sortir");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(llistarButton, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
-            .addComponent(crudAnimalButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(crudAlimentButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(sortirButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(llistarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
-                .addComponent(crudAnimalButton, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(63, 63, 63)
-                .addComponent(crudAlimentButton, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(82, 82, 82)
-                .addComponent(sortirButton)
-                .addGap(39, 39, 39))
-        );
+        sortirButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sortirButtonActionPerformed(evt);
+            }
+        });
+        jPanel2.add(sortirButton, java.awt.BorderLayout.PAGE_END);
 
         jPanel3.setBackground(new java.awt.Color(24, 29, 47));
+        jPanel3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+        jPanel3.setLayout(new java.awt.CardLayout());
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 689, Short.MAX_VALUE)
+        AlimentPanel.setBackground(new java.awt.Color(227, 24, 24));
+
+        javax.swing.GroupLayout AlimentPanelLayout = new javax.swing.GroupLayout(AlimentPanel);
+        AlimentPanel.setLayout(AlimentPanelLayout);
+        AlimentPanelLayout.setHorizontalGroup(
+            AlimentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 661, Short.MAX_VALUE)
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+        AlimentPanelLayout.setVerticalGroup(
+            AlimentPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 598, Short.MAX_VALUE)
         );
+
+        jPanel3.add(AlimentPanel, "card2");
+
+        AnimalPanel.setBackground(new java.awt.Color(24, 227, 42));
+
+        javax.swing.GroupLayout AnimalPanelLayout = new javax.swing.GroupLayout(AnimalPanel);
+        AnimalPanel.setLayout(AnimalPanelLayout);
+        AnimalPanelLayout.setHorizontalGroup(
+            AnimalPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 661, Short.MAX_VALUE)
+        );
+        AnimalPanelLayout.setVerticalGroup(
+            AnimalPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 598, Short.MAX_VALUE)
+        );
+
+        jPanel3.add(AnimalPanel, "card2");
+
+        llistarPanel.setBackground(new java.awt.Color(220, 227, 24));
+
+        javax.swing.GroupLayout llistarPanelLayout = new javax.swing.GroupLayout(llistarPanel);
+        llistarPanel.setLayout(llistarPanelLayout);
+        llistarPanelLayout.setHorizontalGroup(
+            llistarPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 661, Short.MAX_VALUE)
+        );
+        llistarPanelLayout.setVerticalGroup(
+            llistarPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 598, Short.MAX_VALUE)
+        );
+
+        jPanel3.add(llistarPanel, "card2");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -109,7 +156,7 @@ public class MainFrame extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 602, Short.MAX_VALUE)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
@@ -120,7 +167,7 @@ public class MainFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 2, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -134,7 +181,45 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void llistarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_llistarButtonActionPerformed
         // TODO add your handling code here:
+        jPanel3.removeAll();
+        jPanel3.revalidate();
+        jPanel3.add(llistarPanel);
+        jPanel3.revalidate();
+        jPanel3.repaint();
     }//GEN-LAST:event_llistarButtonActionPerformed
+
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formWindowClosed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:       
+        guardaFitxer();
+    }//GEN-LAST:event_formWindowClosing
+
+    private void sortirButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sortirButtonActionPerformed
+        // TODO add your handling code here:
+        this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+        
+    }//GEN-LAST:event_sortirButtonActionPerformed
+
+    private void crudAlimentButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_crudAlimentButtonActionPerformed
+        // TODO add your handling code here:
+        jPanel3.removeAll();
+        jPanel3.revalidate();
+        jPanel3.add(AlimentPanel);
+        jPanel3.revalidate();
+        jPanel3.repaint();
+    }//GEN-LAST:event_crudAlimentButtonActionPerformed
+
+    private void crudAnimalButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_crudAnimalButtonActionPerformed
+        // TODO add your handling code here:
+        jPanel3.removeAll();
+        jPanel3.revalidate();
+        jPanel3.add(AnimalPanel);
+        jPanel3.revalidate();
+        jPanel3.repaint();
+    }//GEN-LAST:event_crudAnimalButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -173,12 +258,19 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel AlimentPanel;
+    private javax.swing.JPanel AnimalPanel;
     private javax.swing.JButton crudAlimentButton;
     private javax.swing.JButton crudAnimalButton;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JButton llistarButton;
+    private javax.swing.JPanel llistarPanel;
     private javax.swing.JButton sortirButton;
     // End of variables declaration//GEN-END:variables
+
+    private void guardaFitxer() {
+        JOptionPane.showMessageDialog(rootPane, "No implementat");
+    }
 }
