796e52bcbd2d26932ef77a84ff33a1d29743eca5
David Martinez <davidmgilo@gmail.com>
2016-11-21 17:12:48 +0100

    added classes to work with following ModelTaula requirements

---
+++ src/projecteuf5gui/Aliment.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteuf5gui;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author alumne
+ */
+public class Aliment implements Comparable{ 
+    private String _1nom;
+    private String _2tipus;
+    private int _3cal; 
+    static final Comparator<Aliment> CAL_ORDER =
+            new Comparator<Aliment> (){
+        @Override
+        public int compare(Aliment a1, Aliment a2) {
+            return (a1.get3cal() < a2.get3cal() ? -1 :
+                    (a1.get3cal() == a2.get3cal() ? 0 : 1));
+        }
+    };
+    static final Comparator<Aliment> TYPE_ORDER =
+            new Comparator<Aliment> (){
+        @Override
+        public int compare(Aliment a1, Aliment a2) {
+            return a1.get2tipus().compareTo(a2.get2tipus());
+        }
+    };
+
+    public Aliment(String nom, String tipus, int cal) {
+        this._1nom = nom;
+        this._2tipus = tipus;
+        this._3cal = cal;
+    }
+
+    public String get1nom() {
+        return _1nom;
+    }
+
+    public void set1nom(String _1nom) {
+        this._1nom = _1nom;
+    }
+
+    public int get3cal() {
+        return _3cal;
+    }
+
+    public void set3cal(int _3cal) {
+        this._3cal = _3cal;
+    }
+
+    public String get2tipus() {
+        return _2tipus;
+    }
+
+    public void set2tipus(String _2tipus) {
+        this._2tipus = _2tipus;
+    }
+
+    @Override
+    public String toString() {
+        return "Aliment{" + "nom=" + _1nom + ", tipus=" + _2tipus + ", cal=" + _3cal + '}';
+    }
+        
+    @Override
+    public int compareTo(Object o) {
+        Aliment a = (Aliment)o;
+        return _1nom.compareTo(a.get1nom());
+    }
+}
---
+++ src/projecteuf5gui/Animal.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteuf5gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alumne
+ */
+public class Animal {
+    private String _1nom;
+    private String _2tipus;
+    private double _3pes;
+    private double _4email_prop;
+    //Ho faig en ArrayList perquè vull elements repetits i és més ràpid d'accés que una LinkedList.
+    private List<Aliment> _5menja = new ArrayList<>();
+
+    public Animal(String _1nom, String _2tipus, double _3pes, double _4email_prop) {
+        this._1nom = _1nom;
+        this._2tipus = _2tipus;
+        this._3pes = _3pes;
+        this._4email_prop = _4email_prop;
+    }
+
+    
+    
+    public String get1nom() {
+        return _1nom;
+    }
+
+    public void set1nom(String _1nom) {
+        this._1nom = _1nom;
+    }
+
+    public String get2tipus() {
+        return _2tipus;
+    }
+
+    public void set2tipus(String _2tipus) {
+        this._2tipus = _2tipus;
+    }
+
+    public double get3pes() {
+        return _3pes;
+    }
+
+    public void set3pes(double _3pes) {
+        this._3pes = _3pes;
+    }
+
+    public double get4email_prop() {
+        return _4email_prop;
+    }
+
+    public void set4email_prop(double _4email_prop) {
+        this._4email_prop = _4email_prop;
+    }
+
+    public List<Aliment> get5menja() {
+        return _5menja;
+    }
+
+    public void set5menja(List<Aliment> _5menja) {
+        this._5menja = _5menja;
+    }
+  
+    
+}
